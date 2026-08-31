c2f52102bcd2940969f68ffff7d560b192564fac
ismailkorkmaz6806 <ismailkorkmaz6806@gmail.com>
2026-08-31 11:46:50 +0300

    refactor: Streamline master menu to 5 core operations

--- BASLAT.bat
+++ BASLAT.bat
@@ -7,65 +7,69 @@ echo =============================================================
 echo    ?? MASt3R-3DGS T?NEL VE DRON HAR?TALAMA MERKEZ?
 echo =============================================================
 echo.
-echo  [1] ?? Drondan Gelen Videoyu Kaydet & 3B Haritaya D?n??t?r
-echo  [2] ?? Webcam ile Test Kayd? Al & 3B Harita ??kar
-echo  [3] ?? Haz?r 3B Ofis Modelini A? (144+ FPS Gezgin)
-echo  [4] ?? ?rnek Ofis Videosunu Ba?tan 3B Modele ?evir
-echo  [5] ?? ?oklu Koridorlar? Tek Haritada Birle?tir
-echo  [6] ?? T?nel ?nceleme ve PDF Raporu ?ret
+echo  [1] ?? Canl? Dron / Webcam ile Tara & 3B Harita ??kar
+echo  [2] ?? Bir MP4 Videosunu 3B Modele D?n??t?r
+echo  [3] ?? 3B Haritay? A? (144+ FPS Gezgin)
+echo  [4] ?? ?oklu Koridorlar? Tek Haritada Birle?tir
+echo  [5] ?? T?nel ?nceleme ve PDF Raporu ?ret
 echo  [0] ? ??k??
 echo.
 echo =============================================================
-set /p SECIM=L?tfen bir i?lem se?in [0-6]: 
+set /p SECIM=L?tfen bir i?lem se?in [0-5]: 
 
-if "%SECIM%"=="1" goto DRON_KAYIT
-if "%SECIM%"=="2" goto WEBCAM_TEST
-if "%SECIM%"=="3" goto OFIS_AC
-if "%SECIM%"=="4" goto OFIS_ISLE
-if "%SECIM%"=="5" goto BIRLESTIR
-if "%SECIM%"=="6" goto RAPOR
+if "%SECIM%"=="1" goto CANLI
+if "%SECIM%"=="2" goto VIDEO_ISLE
+if "%SECIM%"=="3" goto GORUNTULE
+if "%SECIM%"=="4" goto BIRLESTIR
+if "%SECIM%"=="5" goto RAPOR
 if "%SECIM%"=="0" exit
 goto MENU
 
-:DRON_KAYIT
+:CANLI
 cls
 echo =============================================================
-echo  ?? DRON CANLI YAYININDAN V?DEO KAYDI VE 3B HAR?TALAMA
+echo  ?? CANLI DRON / KAMERA ?LE TARAMA
 echo =============================================================
 echo.
-set /p RTSP_URL=Dron RTSP Linki (Varsay?lan: rtsp://192.168.1.100:8554/stream): 
+echo  [1] Laptop / USB Web Kameras? (#0)
+echo  [2] Dron / RTSP Canl? Yay?n? (Wi-Fi / Fiber)
+echo.
+set /p K_SECIM=Se?iminiz [1 veya 2]: 
+if "%K_SECIM%"=="2" goto CANLI_RTSP
+goto CANLI_WEBCAM
+
+:CANLI_RTSP
+echo.
+set /p RTSP_URL=RTSP Linki (Varsay?lan: rtsp://192.168.1.100:8554/stream): 
 if "%RTSP_URL%"=="" set RTSP_URL=rtsp://192.168.1.100:8554/stream
 python live_drone_capture.py "%RTSP_URL%"
 pause
 goto MENU
 
-:WEBCAM_TEST
-cls
-echo =============================================================
-echo  ?? WEBCAM ?LE TEST KAYDI VE 3B HAR?TALAMA
-echo =============================================================
-echo.
+:CANLI_WEBCAM
 python live_drone_capture.py 0
 pause
 goto MENU
 
-:OFIS_AC
+:VIDEO_ISLE
 cls
 echo =============================================================
-echo  ?? 3B OF?S MODEL? A?ILIYOR (144+ FPS)...
+echo  ?? MP4 V?DEOSUNDAN 3B HAR?TA ?RET?M?
 echo =============================================================
 echo.
+set /p V_NAME=??lenecek video ad? veya yolu (Varsay?lan: ofisvideo.mp4): 
+if "%V_NAME%"=="" set V_NAME=ofisvideo.mp4
+python mast3r_to_3dgs.py "%V_NAME%" 50
 python gaussian_renderer.py gaussian_scene.ply
 pause
 goto MENU
 
-:OFIS_ISLE
+:GORUNTULE
 cls
 echo =============================================================
-echo  ?? OF?S V?DEOSU BA?TAN 3B MODELE D?N??T?R?L?YOR...
+echo  ?? 3B MODEL G?R?NT?LEY?C? A?ILIYOR...
 echo =============================================================
 echo.
-python mast3r_to_3dgs.py ofisvideo.mp4 50
 python gaussian_renderer.py gaussian_scene.ply
 pause
 goto MENU
